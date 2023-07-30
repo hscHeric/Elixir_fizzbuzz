23018cab0fccb14fae1194e87410195c416aaf25
Heric da Silva Cruz <hericsilvaho@gmail.com>
2023-07-30 17:45:32 -0300

    FizzBuzz

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,18 +1,31 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
+ def run(file_name) do
+  file_name
+  |> File.read()
+  |> handle_file_read()
+ end
 
-  @doc """
-  Hello world.
+ defp handle_file_read({:ok, result}) do
+  result = result
+  |> String.replace(" ", "")
+  |> String.split(",")
+  |> Enum.map(&convert_and_evaluate_numbers/1)
 
-  ## Examples
+  {:ok, result}
+ end
 
-      iex> FizzBuzz.hello()
-      :world
+ defp handle_file_read({:error, reason}), do: {:error,"Error reading the file: #{reason}"}
 
-  """
-  def hello do
-    :world
-  end
+
+ defp convert_and_evaluate_numbers(elem) do
+  elem
+  |>String.to_integer()
+  |> evaluate_numbers()
+ end
+
+ defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+ defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+ defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+ defp evaluate_numbers(number), do: number
+
 end
